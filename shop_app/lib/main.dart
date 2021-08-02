dbc75f64d7540963266514ccf6cf1cb64699e957
“ameyd96” <erameydeshpande@gmail.com>
2021-08-02 14:19:15 +0530

    Attched order to specific user

--- shop_app/lib/main.dart
+++ shop_app/lib/main.dart
@@ -43,6 +43,7 @@ class MyApp extends StatelessWidget {
             //create: (ctx) => Products(),
             update: (ctx, auth, previousOrders) => Orders(
               auth.token,
+              auth.userId,
               previousOrders == null ? [] : previousOrders.orders,
             ),
           ),
