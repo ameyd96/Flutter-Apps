ea5a6563e56129aaccd23c0a1da1c84272f3910b
“ameyd96” <erameydeshpande@gmail.com>
2021-08-03 14:44:59 +0530

    Image pickup on device is done

--- native_features/lib/widgets/image_input.dart
+++ native_features/lib/widgets/image_input.dart
@@ -1,6 +1,7 @@
 import 'dart:io';
 
 import 'package:flutter/material.dart';
+import 'package:image_picker/image_picker.dart';
 
 class ImageInput extends StatefulWidget {
   @override
@@ -9,6 +10,14 @@ class ImageInput extends StatefulWidget {
 
 class _ImageInputState extends State<ImageInput> {
   File _storedImage;
+
+  Future<void> _takePicture() async{
+    final picker = ImagePicker();
+    final imageFile = await picker.getImage(source: ImageSource.camera,maxWidth: 600,);
+    //final imageFile = ImagePicker.pickImage(source: source)
+  
+  }
+
   @override
   Widget build(BuildContext context) {
     return Row(
@@ -33,7 +42,7 @@ class _ImageInputState extends State<ImageInput> {
             icon: Icon(Icons.camera),
             label: Text('take Picture'),
             textColor: Theme.of(context).primaryColor,
-            onPressed: () {},
+            onPressed: _takePicture,
           ),
         ),
       ],
