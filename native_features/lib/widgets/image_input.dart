29fa34253ceb3905538938ab5d32345a2e5298a3
“ameyd96” <erameydeshpande@gmail.com>
2021-08-03 17:43:46 +0530

    Adding image,text and saving on device storage(Not working on Emulator )

--- native_features/lib/widgets/image_input.dart
+++ native_features/lib/widgets/image_input.dart
@@ -2,8 +2,14 @@ import 'dart:io';
 
 import 'package:flutter/material.dart';
 import 'package:image_picker/image_picker.dart';
+import 'package:path/path.dart' as path;
+import 'package:path_provider/path_provider.dart' as syspaths;
 
 class ImageInput extends StatefulWidget {
+final Function onSelectImage;
+
+ImageInput(this.onSelectImage);
+
   @override
   _ImageInputState createState() => _ImageInputState();
 }
@@ -11,11 +17,31 @@ class ImageInput extends StatefulWidget {
 class _ImageInputState extends State<ImageInput> {
   File _storedImage;
 
-  Future<void> _takePicture() async{
-    final picker = ImagePicker();
-    final imageFile = await picker.getImage(source: ImageSource.camera,maxWidth: 600,);
-    //final imageFile = ImagePicker.pickImage(source: source)
-  
+  Future<void> _takePicture() async {
+    // final picker = ImagePicker();
+    // final imageFile = await picker.getImage(
+    //   source: ImageSource.camera,
+    //   maxWidth: 600,
+    // );
+    final imageFile = await ImagePicker.pickImage(
+      source: ImageSource.camera,
+      maxWidth: 600,
+    );
+    if(imageFile == null){
+      return;
+    }
+    setState(() {
+      _storedImage = imageFile;
+    });
+    // setState(() {
+    //   _storedImage = File(imageFile.path);
+    // });
+
+    final appDir = await syspaths.getApplicationDocumentsDirectory();
+    final fileName = path.basename(imageFile.path);
+    final savedImage = imageFile.copy('${appDir.path}/$fileName');
+
+    widget.onSelectImage(savedImage);
   }
 
   @override
@@ -33,10 +59,15 @@ class _ImageInputState extends State<ImageInput> {
                   fit: BoxFit.cover,
                   width: double.infinity,
                 )
-              : Text('No image taken',textAlign: TextAlign.center,),
+              : Text(
+                  'No image taken',
+                  textAlign: TextAlign.center,
+                ),
           alignment: Alignment.center,
         ),
-        SizedBox(width: 10,),
+        SizedBox(
+          width: 10,
+        ),
         Expanded(
           child: FlatButton.icon(
             icon: Icon(Icons.camera),
